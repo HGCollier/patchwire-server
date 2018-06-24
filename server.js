a6fa2f7a12c94925e588d84691cd9445150f0f7c
unknown <haldengcollier@gmail.com>
2018-06-24 22:22:53 +0100

    The first commit of the Allier server, using Patchworks framework

---
+++ server.js
@@ -0,0 +1,24 @@
+// MyGameServer.js
+const Server = require('patchwire').Server;
+const ClientManager = require('patchwire').ClientManager;
+
+const clientManager = new ClientManager();
+
+clientManager.addCommandListener('pos', function(client, data) {
+    clientManager.broadcast('pos', {
+        x: data.x,
+        y: data.y
+    });
+});
+
+clientManager.on('clientAdded', function() {
+    clientManager.broadcast('chat', {
+        message: 'A new player has joined the game.'
+    });
+});
+
+const server = new Server(function(client) {
+    clientManager.addClient(client);
+});
+
+server.listen(3001);
